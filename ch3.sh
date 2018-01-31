98b4fa4eeda74d59db299cbd3c0551272b92b508
Debian Live user <amnesia@localhost>
2018-01-31 22:43:30 +0000

    finished ch3 hw

---
+++ ch3.sh
@@ -0,0 +1,20 @@
+#Networking 3 Linux
+#Alex Malm
+#Ch. 3
+cd ~/Documents
+
+$echo "ATA_ID is exported to a file ATAid.txt using sudo"
+touch ATAid.txt
+sudo /lib/udev/ata_id --export /dev/sda | ATAid.txt
+head ATAid.txt
+$echo "Current Directory is changed to documents, and output from udevadm is piped to devices.txt"
+touch devices.txt
+udevadm info --query=all --name=/dev/sda | devices.txt
+head devices.txt
+
+$echo "Next, we will pipe the udevadm monitor to 'more' "
+
+udevadm monitor | more
+
+rm ATAid.txt,devices.txt
+cd ~
