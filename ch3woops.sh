07c47f2504d716892a3ade58c61300f649e12cc2
Debian Live user <amnesia@localhost>
2018-01-31 23:32:21 +0000

    adding backup file

---
+++ ch3woops.sh
@@ -0,0 +1,21 @@
+#Networking 3 Linux
+#Alex Malm
+#Ch. 3
+sudo cd ~/Documents
+
+$echo "ATA_ID is exported to a file ATAid.txt using sudo"
+touch ATAid.txt
+/lib/udev/ata_id --export /dev/sda | ATAid.txt
+head ATAid.txt
+$echo "Current Directory is changed to documents, and output from udevadm is piped to devices.txt"
+touch devices.txt
+udevadm info --query=all --name=/dev/sda | devices.txt
+head devices.txt
+
+$echo "Next, we will pipe the udevadm monitor to 'more'. "
+
+udevadm monitor | more
+
+rm ATAid.txt
+rm devices.txt
+cd ~
