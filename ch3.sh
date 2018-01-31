412cc8dcfa01e61b7b24078273eabc1ddc948c43
The Malmster <boombot101@gmail.com>
2018-01-31 23:25:29 +0000

    Update ch3.sh

--- ch3.sh
+++ ch3.sh
@@ -1,20 +1,21 @@
 #Networking 3 Linux
 #Alex Malm
 #Ch. 3
-cd ~/Documents
+sudo cd ~/Documents
 
 $echo "ATA_ID is exported to a file ATAid.txt using sudo"
 touch ATAid.txt
-sudo /lib/udev/ata_id --export /dev/sda | ATAid.txt
+/lib/udev/ata_id --export /dev/sda | ATAid.txt
 head ATAid.txt
 $echo "Current Directory is changed to documents, and output from udevadm is piped to devices.txt"
 touch devices.txt
 udevadm info --query=all --name=/dev/sda | devices.txt
 head devices.txt
 
-$echo "Next, we will pipe the udevadm monitor to 'more' "
+$echo "Next, we will pipe the udevadm monitor to 'more'. "
 
 udevadm monitor | more
 
-rm ATAid.txt,devices.txt
+rm ATAid.txt
+rm devices.txt
 cd ~
