3fa9722f000b95c03792c1316739ac2b8eb8b704
rarar0/0 <boombot101@gmail.com>
2018-03-07 03:02:01 -0600

    Update ch3.sh

--- ch3.sh
+++ ch3.sh
@@ -3,16 +3,16 @@
 #Ch. 3
 sudo cd ~/Documents
 
-$echo "ATA_ID is exported to a file ATAid.txt using sudo"
+echo "ATA_ID is exported to a file ATAid.txt using sudo"
 touch ATAid.txt
 /lib/udev/ata_id --export /dev/sda | ATAid.txt
 head ATAid.txt
-$echo "Current Directory is changed to documents, and output from udevadm is piped to devices.txt"
+echo "Current Directory is changed to documents, and output from udevadm is piped to devices.txt"
 touch devices.txt
 udevadm info --query=all --name=/dev/sda | devices.txt
 head devices.txt
 
-$echo "Next, we will pipe the udevadm monitor to 'less'. "
+echo "Next, we will pipe the udevadm monitor to 'less'. "
 
 udevadm monitor | less
 
